2f0c0084581d4445886cb579ab9e47e4e8e6b147
Morgana Weber <50406261+morgsweber@users.noreply.github.com>
2022-08-15 18:47:52 -0300

    Formating files

--- src/udp/UDPClient.java
+++ src/udp/UDPClient.java
@@ -2,19 +2,18 @@ import java.net.*;
 import java.io.*;
 public class UDPClient
 {
-            public static void main(String args[])throws Exception
-            {         
-                        byte b[]=new byte[1024];
-                        FileInputStream f=new FileInputStream("teste.txt");
-                        DatagramSocket dsoc=new DatagramSocket(2000);
-                        int i=0;
-                        while(f.available()!=0)
-                        {
-                                    b[i]=(byte)f.read();
-                                    i++;
-                        }                     
-                        f.close();
-                        dsoc.send(new DatagramPacket(b,i,InetAddress.getLocalHost(),1000));
-            }
-
+    public static void main(String args[])throws Exception
+    {         
+        byte b[]=new byte[1024];
+        FileInputStream f = new FileInputStream("teste.txt");
+        DatagramSocket dsoc = new DatagramSocket(2000);
+        int i = 0;
+        while(f.available()!=0)
+        {
+            b[i] = (byte)f.read();
+            i++;
+        }                     
+        f.close();
+        dsoc.send(new DatagramPacket(b,i,InetAddress.getLocalHost(),1000));
+    }
 }
--- src/udp/UDPServer.java
+++ src/udp/UDPServer.java
@@ -2,17 +2,16 @@ import java.net.*;
 import java.io.*;
 public class UDPServer
 {
-            public static void main(String args[])throws IOException
-            {
-                        byte b[]=new byte[3072];
-                        DatagramSocket dsoc=new DatagramSocket(1000);
-                        FileOutputStream f=new FileOutputStream("teste2.txt");
-                        while(true)
-                        {
-                                    DatagramPacket dp=new DatagramPacket(b,b.length);
-                                    dsoc.receive(dp);
-                                    System.out.println(new String(dp.getData(),0,dp.getLength()));                             
-
-                        }
-            }
+    public static void main(String args[])throws IOException
+    {
+        byte b[] = new byte[3072];
+        DatagramSocket dsoc = new DatagramSocket(1000);
+        FileOutputStream f = new FileOutputStream("teste2.txt");
+        while(true)
+        {
+            DatagramPacket dp = new DatagramPacket(b,b.length);
+            dsoc.receive(dp);
+            System.out.println(new String(dp.getData(),0,dp.getLength()));                             
+        }
+    }
 }
